2de13c1fee1e8f88b0f30f466ffd2bccea572508
Quinton <quinton.bel@hotmail.com>
2019-03-02 17:04:16 -0500

    Finished UserManager Functionality

--- FrontEndRapidPrototype/UserManager.cpp
+++ FrontEndRapidPrototype/UserManager.cpp
@@ -1,16 +1,52 @@
 #include "UserManager.h"
+#include <fstream>
 
 static User getUser(std::string name){
-    //Gets the user data from the file
+    double credit;
+    std::string filetype;
+    std::string fileuname;
 
+    //Gets the user data from the file
+    std::string line;
+    std::ifstream usersFile;
+    usersFile.open("CurrentUsers.txt");
+    if(usersFile.is_open()){
+        while( std::getline(usersFile, line)){
+            //Splits the data from the line
+            fileuname = line.substr(0, 15);
+            filetype = line.substr(16, 2);
+            credit = std::stod(line.substr(19, 9));
+
+            //Checks if its the right record
+            if(name.compare(fileuname)==0){
+                break;
+            }
+        }
+        usersFile.close();
+    }
     //Sets the user data to return
     User user;
-    user.addCredit(999.99);
-    user.setName("Test Name");
+    user.addCredit(credit);
+    user.setName(fileuname);
     Type type = AA;
     user.setType(type);
     return user;
 };
 static bool exists(std::string name){
-    return true;
+    //Gets the user data from the file
+    std::string line;
+    std::string fileuname;
+    std::ifstream usersFile;
+    usersFile.open("CurrentUsers.txt");
+    if(usersFile.is_open()){
+        while( std::getline(usersFile, line)){
+            fileuname = line.substr(0, 15);
+            //Checks if its the right record
+            if(name.compare(fileuname)==0){
+                return true;
+            }
+        }
+        usersFile.close();
+    }
+    return false;
 };
